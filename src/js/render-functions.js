9a9dd75d72372bb041a20a51d5717d822b5e9ad1
Evgeniy-Popov-98 <evgeniy6183@gmail.com>
2024-02-09 22:58:45 +0200

    first

---
+++ src/js/render-functions.js
@@ -0,0 +1,33 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+
+export default function renderImages(images) {
+  loader.setAttribute('hidden', '');
+  const markup = images
+    .map(img => {
+      return `<li class="gallery-item">
+		   <a class="gallery-link" href=${img.largeImageURL}>
+			   <img class="gallery-image" src=${img.webformatURL} alt="${img.tags}" />
+		   </a>
+		   <div class="container">
+			   <div class="container-text"><h3 class="titel-text">Likes</h3><p class="text">${img.likes}</p></div>
+			   <div class="container-text"><h3 class="titel-text">Views</h3><p class="text">${img.views}</p></div>
+			   <div class="container-text"><h3 class="titel-text">Comments</h3><p class="text">${img.comments}</p></div>
+			   <div class="container-text"><h3 class="titel-text">Dowloads</h3><p class="text">${img.downloads}</p></div>
+		   </div>
+	   </li>`;
+    })
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', markup);
+
+  let galleryModalWindow = new SimpleLightbox('.gallery-link', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+  });
+
+  galleryModalWindow.refresh();
+}
--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-/*========================================*/
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 /*=============================================================*/
+import renderImages from './js/render-functions';
+import { infoAPI } from './js/pizsbay-api';
 
 const form = document.querySelector('.js-form');
 const input = document.querySelector('.js-input');
 const buttom = document.querySelector('.js-button');
 const gallery = document.querySelector('.gallery');
 const loader = document.querySelector('.loader');
+
+const newInfoApi = new infoAPI();
 
 form.addEventListener('submit', creatGallery);
 
@@ -20,7 +21,19 @@ function creatGallery(event) {
   loader.removeAttribute('hidden');
   removeGallery();
   if (infoSearch) {
-    getImages(infoSearch);
+    // loader.setAttribute('hidden', '');
+
+    try {
+      console.log(infoSearch);
+      const data = newInfoApi.getInfoArticles(infoSearch);
+      //   console.log(data);
+      //   renderImages(data.hits);
+    } catch (error) {
+      iziToast.error({
+        title: 'Error',
+        message: err.message,
+      });
+    }
   } else {
     iziToast.error({
       position: 'topRight',
@@ -37,58 +50,28 @@ function removeGallery() {
   }
 }
 
-function getImages(infoSearch) {
-  const url = `https://pixabay.com/api/?key=42169950-0e8cca4ed1d3fcef898dec13a&q=${infoSearch}&image_type=photo&orientation=horizontal&safesearch=true`;
+// function getImages(infoSearch) {
+//   const url = `https://pixabay.com/api/?key=42169950-0e8cca4ed1d3fcef898dec13a&q=${infoSearch}&image_type=photo&orientation=horizontal&safesearch=true`;
 
-  return fetch(url)
-    .then(res => {
-      if (res.ok) {
-        return res.json();
-      } else {
-        const myError = new Error(`${res.status}`);
-        throw myError;
-      }
-    })
-    .then(resolt => {
-      if (resolt.hits.length !== 0) {
-        renderImages(resolt.hits);
-      } else {
-        iziToast.error({
-          position: 'topRight',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-        });
-      }
-    })
-    .catch(err => console.log(err.message));
-}
-
-function imageTemplate(img) {
-  return `
-			 <li class="gallery-item">
-				<a class="gallery-link" href=${img.largeImageURL}>
-					<img class="gallery-image" src=${img.webformatURL} alt="${img.tags}" />
-				</a>
-				<div class="container">
-					<div class="container-text"><h3 class="titel-text">Likes</h3><p class="text">${img.likes}</p></div>
-					<div class="container-text"><h3 class="titel-text">Views</h3><p class="text">${img.views}</p></div>
-					<div class="container-text"><h3 class="titel-text">Comments</h3><p class="text">${img.comments}</p></div>
-					<div class="container-text"><h3 class="titel-text">Dowloads</h3><p class="text">${img.downloads}</p></div>
-				</div>
-			</li>
-			`;
-}
-
-function renderImages(images) {
-  loader.setAttribute('hidden', '');
-  const markup = images.map(imageTemplate).join('');
-  gallery.innerHTML = markup;
-
-  let galleryModalWindow = new SimpleLightbox('.gallery-link', {
-    captionsData: 'alt',
-    captionPosition: 'bottom',
-    captionDelay: 250,
-  });
-
-  galleryModalWindow.refresh();
-}
+//   return fetch(url)
+//     .then(res => {
+//       if (res.ok) {
+//         return res.json();
+//       } else {
+//         const myError = new Error(`${res.status}`);
+//         throw myError;
+//       }
+//     })
+//     .then(resolt => {
+//       if (resolt.hits.length !== 0) {
+//         renderImages(resolt.hits);
+//       } else {
+//         iziToast.error({
+//           position: 'topRight',
+//           message:
+//             'Sorry, there are no images matching your search query. Please try again!',
+//         });
+//       }
+//     })
+//     .catch(err => console.log(err.message));
+// }
